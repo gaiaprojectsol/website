0baadb79761e35be0a61d90ff451c51a8a729a14
gaiaprojectsol <gaiaprojectsol@gmail.com>
2025-11-24 14:42:17 +1030

    Add configuration settings for game parameters

---
+++ chaosJump/js/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+SPRITE_SIZE:48, GRAVITY:0.6, BASE_SPEED:6, BASE_JUMP:12,
+AWS_ENDPOINT:"https://xrrn4dg2d4.execute-api.ap-southeast-2.amazonaws.com/default/genean-score-validator",
+AWS_LEADERBOARD_ENDPOINT:"https://xrrn4dg2d4.execute-api.ap-southeast-2.amazonaws.com/default/genean-score-validator"
+};
